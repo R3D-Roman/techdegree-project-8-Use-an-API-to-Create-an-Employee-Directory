d570c74259ab16daac1fcccef8be90920ffa1fe1
Roman Rudyk <patrikpappy@gmai.com>
2020-01-04 14:33:46 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -162,14 +162,6 @@ window.addEventListener("click", (e) => {
 
 });
 
-let arrowRight = document.querySelector(".arrow-right") === null;
- arrowRight.addEventListener("click", (e) => { 
-if(arrowRight != null) {
-    console.log(arrowRight);
-      right();
-}
-    });
-
 
 
 
