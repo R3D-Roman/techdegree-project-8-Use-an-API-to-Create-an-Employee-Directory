6e54c62359050045552184ad97c861d44c900f36
Roman Rudyk <patrikpappy@gmai.com>
2020-01-04 19:00:12 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -162,7 +162,7 @@ window.addEventListener("click", (e) => {
 
 });
 
-document.addEventListener("touchstart", (e) => {
+document.addEventListener("touchend", (e) => {
 
     if (e.target.classList.contains('arrow-left')) {
         // left arrow
